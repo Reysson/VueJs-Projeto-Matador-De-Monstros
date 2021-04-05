bfd078a64479a94b8100ccc2d33d96bfb0b0e487
reysson <reysson.joseph29@gmail.com>
2021-04-05 15:44:57 -0400

    Implementando a funcao atacar

--- Matador de Monstros/app.js
+++ Matador de Monstros/app.js
@@ -15,9 +15,27 @@ new Vue({
             this.playerLife = 100
             this.monsterLife = 100
             this.running = true
+        },
+        attack(especial) {
+            this.hurt('playerLife', 7, 10, especial)
+            this.hurt('monsterLife', 7, 10, especial)
+        },
+
+        hurt(propri, min, max, especial) {
+            let plus = especial ? 5 : 0
+            let hurt = this.getRandom(min + plus, max + plus)
+            this[propri] = Math.max(this[propri] - hurt, 0)
+        },
+
+        getRandom(min, max) {
+            console.log(min, max)
+            let value = Math.random() * (max - min) + min
+            return Math.round(value)
         }
     },
     watch: {
-
+        hasResult(value) {
+            if (value) this.running = false
+        }
     }
 })
